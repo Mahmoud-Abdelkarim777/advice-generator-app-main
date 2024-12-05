c4f4e39da6c2254cb1077eb4bb0da5a171e17946
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2024-12-05 04:53:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let Id_slip = document.getElementById('id-slip');
+let Advice = document.getElementById('advice');
+let Img = document.getElementById('img');
+
+async function getUser() {
+    try {
+        const response = await axios.get(`https://api.adviceslip.com/advice`);
+        const advice = response.data.slip
+        console.log(advice);
+        Id_slip.innerHTML = "";
+        Advice.innerHTML = "";
+        Id_slip.innerHTML  = advice.id
+        Advice.innerHTML  = advice.advice
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+        });
+    }
+}
+
+Img.addEventListener("click", getUser);
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./main.js","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        manrope: ['Manrope', 'sans-serif'], 
+      },
+      colors: {
+        primary: {
+          cyan: 'hsl(193, 38%, 86%)', // Light Cyan
+          neon: 'hsl(150, 100%, 66%)', // Neon Green
+        },
+        neutral: {
+            grayishBlue: 'hsl(217, 19%, 38%)', // Grayish Blue
+            darkGrayishBlue: 'hsl(217, 19%, 24%)', // Dark Grayish Blue
+            darkBlue: 'hsl(218, 23%, 16%)', // Dark Blue
+          },
+      },
+      height: {
+        '2/2': '100vh',  
+        '17': '16.5rem',  
+      },
+      inset: {
+        'custom': '14.7rem', 
+      },
+      fontSize: {
+        'custom': '19px', // حجم الخط المخصص
+      },
+      boxShadow: {
+        'neon-green': '0px 0px 25px 5px hsl(150deg 100% 62.08%)',
+      },
+    },
+    letterSpacing: {
+      wide: '4px',
+    },
+  },
+  plugins: [],
+}
