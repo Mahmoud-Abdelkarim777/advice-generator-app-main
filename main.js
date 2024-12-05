3731bfe0ba75ce037bbc93b79f2bc58e13f246a9
Mahmoud-Abdelkarim777 <mabdelkarimkhalaf777@gmail.com>
2024-12-05 21:19:58 +0200

    first commit

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ let Img = document.getElementById('img');
 
 async function getUser() {
     try {
-        const response = await axios.get(`https://api.adviceslip.com/advice`);
+        const response = await axios.get(`https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`);
         const advice = response.data.slip
         console.log(advice);
-        Id_slip.innerHTML = "";
-        Advice.innerHTML = "";
+        Id_slip.innerHTML = "Loading...";
+        Advice.innerHTML = "Please wait while we fetch the advice...";
         Id_slip.innerHTML  = advice.id
         Advice.innerHTML  = advice.advice
     } catch (error) {
